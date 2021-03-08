7bc9822d0a9b7eab179b00837f0eeae3126d913b
Naveen Doddaiah <43662840+naveen-realm@users.noreply.github.com>
2021-03-08 08:10:45 +0530

    added comment

--- nested-object-finder.js
+++ nested-object-finder.js
@@ -1,7 +1,7 @@
 var object = {"a1": { "b1": { "c1": "d1" }}};
 
 console.log("Result at 'a1.b1.c1': ",_.get(object, 'a1.b1.c1')); // _.get is from package lodash.js
-console.log("Result at 'a1.b1.nonexistent key': ",_.get(object, 'a1.b1.nonexistent', "default result"));
+console.log("Result at 'a1.b1.nonexistent key': ",_.get(object, 'a1.b1.nonexistent', "default result")); // The default value is returned if the key / path cannot be found. If you don't supply a default value undefined is returned instead. 
 
 var object2 = {"x1":{"y1":{"z1":"a1"}}};
 console.log("Result at 'x1.y1.z1': ",_.get(object2, 'x1.y1.z1')); // outputs a1
